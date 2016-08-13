c1bd8568c9ca678a2b2141261c403da426bf3208
AVataRR626 <matt.cabanag@gmail.com>
2016-08-13 21:30:07 +1000

    dynamic set menu
    implemented dynamic set menu
    graphical changes to editor

---
+++ Crash Chain/Assets/QSIUtils/Camera&GUI/PuzzleMenuGenerator.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.Utility;
+
+public class PuzzleMenuGenerator : MonoBehaviour
+{
+    //set settings
+    [Header("Set Settings")]
+    public int setNumber = 1;
+    public int maxSet = 12;
+
+    //display stuff
+    [Header("Display Settings")]
+    public Text textDisplay;
+    public string prefix = "Set: ";
+
+    //layout stuff
+    [Header("Grid Layout")]
+    public GameObject template;
+    public Transform startPoint;
+    public float xDistance = 3;
+    public float yDistance = 3;
+    public int colCount = 3;
+    public int rowCount = 4;
+
+    //movement stuff
+    [Header("Movement Settings")]
+    public AutoMoveAndRotate autoMover;
+    public float moveAcceleration = 2;
+    public float xSpeed;    
+    public float xMax = 1000;
+    public float xMin = -1000;
+
+    private bool moveMode;
+    private int direction = 1;
+    Vector3 originalPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalPos = transform.localPosition;
+        autoMover = GetComponent<AutoMoveAndRotate>();
+        GenerateButtons();
+
+        DisplayUpdate();
+
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(moveMode)
+        {
+            xSpeed += moveAcceleration * Time.deltaTime * -direction;
+
+            if(transform.localPosition.x < originalPos.x + xMax ^ transform.localPosition.x > originalPos.x + xMin)
+            {
+                xSpeed = 0;
+                moveMode = false;
+                transform.localPosition = originalPos;                
+                DisplayUpdate();
+            }
+
+            autoMover.moveUnitsPerSecond.value.x = xSpeed;
+
+        }
+	}
+
+    public void DisplayUpdate()
+    {
+        textDisplay.text = prefix + setNumber.ToString();
+    }
+
+
+    public void SetMod(int dir)
+    {
+        if(dir > 0)
+        {
+            SetChange(1);
+        }
+
+        if(dir < 0)
+        {
+            SetChange(-1);
+        }
+    }
+
+    void SetChange(int i)
+    {
+        setNumber += i;
+        moveMode = true;
+        direction = i;
+
+        if (setNumber < 1)
+            setNumber = maxSet;
+
+        if (setNumber > maxSet)
+            setNumber = 1;
+    }
+
+    public void GenerateButtons()
+    {
+        Vector3 spawnPos = startPoint.position;
+
+        int puzzleNumber = 1;
+
+        for(int i = 0; i < rowCount; i++)
+        { 
+            for(int j = 0; j < colCount; j++)
+            { 
+                //place the button
+                GameObject b = Instantiate(template, spawnPos, startPoint.rotation) as GameObject;
+                b.transform.parent = transform;
+                
+
+                //set the button settings
+                PuzzleLoader pl = b.GetComponent<PuzzleLoader>();
+
+                if(pl != null)
+                {
+                    pl.setNumber = setNumber;
+                    pl.puzzleNumber = puzzleNumber;
+
+                    Transform label = pl.transform.FindChild("Text");
+
+                    if(label != null)
+                    {
+                        label.GetComponent<Text>().text = puzzleNumber.ToString();
+                    }
+                }
+
+                spawnPos.x += xDistance;
+                puzzleNumber++;
+            }
+            spawnPos.y += yDistance;
+            spawnPos.x = startPoint.position.x;
+        }
+    }
+}
---
+++ Crash Chain/Assets/QSIUtils/LevelLoading/PuzzleLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PuzzleLoader : MonoBehaviour
+{
+    public string puzzlePrefix = "puzzle_";
+    public string delimiter = "_";
+    public int setNumber = 1;
+    public int puzzleNumber = 1;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel(puzzlePrefix + delimiter + setNumber.ToString() + delimiter + puzzleNumber.ToString());
+    }
+}
