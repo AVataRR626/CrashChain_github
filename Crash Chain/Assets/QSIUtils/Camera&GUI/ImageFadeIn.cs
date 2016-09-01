e14ca6ebb2f04662fa71188868181d84ad0ae8d8
AVataRR626 <matt.cabanag@gmail.com>
2016-09-02 00:08:49 +1000

    colour change tutorial tweeks
    fade in colour instructions

--- Crash Chain/Assets/QSIUtils/Camera&GUI/ImageFadeIn.cs
+++ Crash Chain/Assets/QSIUtils/Camera&GUI/ImageFadeIn.cs
@@ -6,29 +6,42 @@ public class ImageFadeIn : MonoBehaviour
 {
     public Image img;
     public Text txt;
+    public TextMesh txtmsh;
     // Use this for initialization
     public float fadeInRate = 1;
     public float startDelay = 0;
+    public bool fadeOutMode = false;
 
-    private float [] originalAlpha = new float[2];
+    private float [] originalAlpha = new float[3];
+    private float startAlpha = 0;
+    
 
 	void Start ()
     {
         img = GetComponent<Image>();
         txt = GetComponent<Text>();
+        txtmsh = GetComponent<TextMesh>();
+
+        if (fadeOutMode)
+            startAlpha = 1;
 
         if (img != null)
         {
             originalAlpha[0] = img.color.a;
-            img.color = SetAlpha(img.color, 0);
+            img.color = SetAlpha(img.color, startAlpha);
         }
 
         if (txt != null)
         {
             originalAlpha[1] = txt.color.a;
-            txt.color = SetAlpha(txt.color, 0);
+            txt.color = SetAlpha(txt.color, startAlpha);
         }
         
+        if(txtmsh != null)
+        {
+            originalAlpha[2] = txtmsh.color.a;
+            txtmsh.color = SetAlpha(txtmsh.color, startAlpha);
+        }
 
     }
 	
@@ -36,9 +49,35 @@ public class ImageFadeIn : MonoBehaviour
 	void Update ()
     {
         if (startDelay <= 0)
-            FadeIn();
+        {
+            if (fadeOutMode)
+                FadeOut();
+            else
+                FadeIn();
+        }
         else
+        {
             startDelay -= Time.deltaTime;
+        }
+    }
+
+    void FadeOut()
+    {
+        Debug.Log("Fade Out:");
+        if (img != null)
+        {
+            img.color = FadeOut(img.color, 0, fadeInRate);
+        }
+
+        if (txt != null)
+        {
+            txt.color = FadeOut(txt.color, 0, fadeInRate);
+        }
+
+        if(txtmsh != null)
+        {
+            txtmsh.color = FadeOut(txtmsh.color, 0, fadeInRate);
+        }
     }
 
     void FadeIn()
@@ -52,12 +91,29 @@ public class ImageFadeIn : MonoBehaviour
         {
             txt.color = FadeIn(txt.color, originalAlpha[1], fadeInRate);
         }
+
+        if (txtmsh != null)
+        {
+            txtmsh.color = FadeIn(txtmsh.color, originalAlpha[2], fadeInRate);
+        }
+    }
+
+    public static Color FadeOut(Color c, float goalAlpha, float increment)
+    {
+        Debug.Log("Fade Out:" + c.a);
+        if (c.a > goalAlpha)
+        {
+            Debug.Log("Yup");
+            c.a -= increment * Time.deltaTime;
+        }
+
+        return c;
     }
 
     public static Color FadeIn(Color c, float goalAlpha, float increment)
     {
         if(c.a < goalAlpha)
-        {
+        {   
             c.a += increment * Time.deltaTime;
         }
 
