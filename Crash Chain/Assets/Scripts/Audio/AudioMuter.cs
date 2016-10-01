2ed3a5f1643e63f34d5196e472eacdd2a4fd6e04
AVataRR626 <matt.cabanag@gmail.com>
2016-10-01 19:45:44 +1000

    tweaks
    -editor now starts with empty scene; highlighter on screen
    -added mute button to zen and arcade modes
    -tweaked note icon

---
+++ Crash Chain/Assets/Scripts/Audio/AudioMuter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioMuter : MonoBehaviour
+{
+
+    public static string muteKeyString = "MuteMusic";
+    bool mute = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if(PlayerPrefs.HasKey(muteKeyString))
+        {
+            int result = PlayerPrefs.GetInt(muteKeyString, 0);
+            mute = (result != 0);
+        }
+
+        if(mute)
+            MuteAll(mute);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void ToggleMute()
+    {
+        SetMute(!mute);
+    }
+
+    public void SetMute(bool m)
+    {
+        mute = m;
+        int prefsVal = 1;
+
+        if (!mute)
+            prefsVal = 0;
+
+        PlayerPrefs.SetInt(muteKeyString, prefsVal);
+        MuteAll(mute);
+    }
+
+    public void MuteAll(bool m)
+    {
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+
+        foreach(AudioSource a in audioSources)
+        {
+            if (m)
+                a.Stop();
+            else
+                a.Play();
+        }
+    }
+
+}
