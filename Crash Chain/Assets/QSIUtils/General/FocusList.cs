e261d687ce6e247c5c25fddc127835997d7a62ef
AVataRR626 <matt.cabanag@gmail.com>
2016-10-31 10:29:06 +1100

    zoooorp

--- Crash Chain/Assets/QSIUtils/General/FocusList.cs
+++ Crash Chain/Assets/QSIUtils/General/FocusList.cs
@@ -11,16 +11,6 @@ public class FocusList : MonoBehaviour
 
     public GameObject[] focusList;
 
-	// Use this for initialization
-	void Start ()
-    {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     public void Focus(int target)
     {
