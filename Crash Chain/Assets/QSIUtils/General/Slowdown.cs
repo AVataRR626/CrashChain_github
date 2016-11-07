6ad1a90adaab33168bdaa2a03d6648e2c4677dc3
AVataRR626 <matt.cabanag@gmail.com>
2016-11-08 08:45:52 +1100

    tweekstweekstweeks

---
+++ Crash Chain/Assets/QSIUtils/General/Slowdown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slowdown : MonoBehaviour
+{
+    public float goalTimescale = 0.5f;
+    public float slowdownRate = 0.01f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.timeScale > goalTimescale)
+        {
+            Time.timeScale -= slowdownRate * Time.deltaTime;
+        }
+        else
+        {
+            Time.timeScale = goalTimescale;
+        }
+	}
+}
