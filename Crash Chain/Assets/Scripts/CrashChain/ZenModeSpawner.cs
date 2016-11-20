cb4ab3ba4cd31b0ae187ae239bb800beff7b1935
AVataRR626 <matt.cabanag@gmail.com>
2016-11-20 16:44:42 +1100

    work on new zen mode tutorial tweeks

---
+++ Crash Chain/Assets/Scripts/CrashChain/ZenModeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZenModeSpawner : MonoBehaviour
+{
+    public int level = 0;
+    public int spawnIndex = 0;
+    public CrashLink[] spawnObjects;
+    public string[] spawnMessages;
+
+    float originalZ;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 newPos;
+
+            newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPos.z = originalZ;
+
+            transform.position = newPos;
+        }
+	}
+
+    void Spawn()
+    {
+        CrashLink c = Instantiate(spawnObjects[spawnIndex], transform.position, Quaternion.identity) as CrashLink;
+    }
+}
